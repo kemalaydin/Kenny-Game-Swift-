32f7a501d095b944885a7c1bb540be1077cd443b
Kemal Aydın <kemalaydin@Kemal-MacBook-Pro.local>
2017-08-26 01:06:22 +0300

    Initial Commit

---
+++ KennyYakala/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KennyYakala
+//
+//  Created by Kemal Aydın on 26.08.2017.
+//  Copyright © 2017 Kemal Aydın. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
